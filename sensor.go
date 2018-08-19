23f6e71c40a83fecb230cbd6d1dbd1e23e15b0c3
Felixls <sergioscarnatto@gmail.com>
2018-08-19 09:35:16 +0100

    Fix sensor types

--- sensor.go
+++ sensor.go
@@ -2,14 +2,14 @@ package huego
 
 // Sensor represents a bridge sensor https://developers.meethue.com/documentation/sensors-api
 type Sensor struct {
-	State            interface{} `json:"state,omitempty"`
-	Config           interface{} `json:"config,omitempty"`
-	Name             string      `json:"name,omitempty"`
-	Type             string      `json:"type,omitempty"`
-	ModelID          string      `json:"modelid,omitempty"`
-	ManufacturerName string      `json:"manufacturername,omitempty"`
-	SwVersion        string      `json:"swversion,omitemptyn"`
-	ID               int         `json:",omitempty"`
+	State            map[string]interface{} `json:"state,omitempty"`
+	Config           map[string]interface{} `json:"config,omitempty"`
+	Name             string                 `json:"name,omitempty"`
+	Type             string                 `json:"type,omitempty"`
+	ModelID          string                 `json:"modelid,omitempty"`
+	ManufacturerName string                 `json:"manufacturername,omitempty"`
+	SwVersion        string                 `json:"swversion,omitemptyn"`
+	ID               int                    `json:",omitempty"`
 }
 
 // NewSensor defines a list of sensors discovered the last time the bridge performed a sensor discovery.
