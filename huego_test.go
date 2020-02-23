f6a31f2b31ea1304bec7d0cbeb0ff1e304977f27
Amir Mofasser <amir.mofasser@gmail.com>
2020-02-23 12:58:31 +0100

    Make `GroupState` field a pointer on type `Group` (#17)
    * Make `GroupState` field a pointer on type `Group`
    
    * Added `state` to group API response in tests

--- huego_test.go
+++ huego_test.go
@@ -108,7 +108,7 @@ func init() {
 		{
 			method: "GET",
 			path:   "/groups",
-			data:   `{"1":{"name":"Group 1","lights":["1","2"],"type":"LightGroup","action":{"on":true,"bri":254,"hue":10000,"sat":254,"effect":"none","xy":[0.5,0.5],"ct":250,"alert":"select","colormode":"ct"}},"2":{"name":"Group 2","lights":["3","4","5"],"type":"LightGroup","action":{"on":true,"bri":153,"hue":4345,"sat":254,"effect":"none","xy":[0.5,0.5],"ct":250,"alert":"select","colormode":"ct"}}}`,
+			data:   `{"1":{"name":"Group 1","lights":["1","2"],"type":"LightGroup","state":{"all_on":true,"any_on":true},"action":{"on":true,"bri":254,"hue":10000,"sat":254,"effect":"none","xy":[0.5,0.5],"ct":250,"alert":"select","colormode":"ct"}},"2":{"name":"Group 2","lights":["3","4","5"],"type":"LightGroup","state":{"all_on":true,"any_on":true},"action":{"on":true,"bri":153,"hue":4345,"sat":254,"effect":"none","xy":[0.5,0.5],"ct":250,"alert":"select","colormode":"ct"}}}`,
 		},
 		{
 			method: "GET",
