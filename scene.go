5eeb7cfa2e746c6b65b9a92308435e4c6138070a
ttgmpsn <36986902+ttgmpsn@users.noreply.github.com>
2021-05-09 02:33:50 +0200

    Remove omitempty from Scene struct
    Even though the doc states "Set to ‘false’ when omitted.", the bridge actually complains when trying to do just that:
    
    ```[
    	{
    		"error": {
    			"type": 5,
    			"address": "/scenes/recycle",
    			"description": "invalid/missing parameters in body"
    		}
    	}
    ]
    ```

--- scene.go
+++ scene.go
@@ -9,7 +9,7 @@ type Scene struct {
 	Group           string        `json:"group,omitempty"`
 	Lights          []string      `json:"lights,omitempty"`
 	Owner           string        `json:"owner,omitempty"`
-	Recycle         bool          `json:"recycle,omitempty"`
+	Recycle         bool          `json:"recycle"`
 	Locked          bool          `json:"locked,omitempty"`
 	AppData         interface{}   `json:"appdata,omitempty"`
 	Picture         string        `json:"picture,omitempty"`
